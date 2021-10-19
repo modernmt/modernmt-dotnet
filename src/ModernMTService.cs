65051080f80ffd93f958b2dd5001b84b6162dec7
Davide Conti <davide.conti@modernmt.eu>
2021-10-19 09:28:45 +0200

    Update version to 1.0.1

--- src/ModernMTService.cs
+++ src/ModernMTService.cs
@@ -14,7 +14,7 @@ namespace ModernMT
         private readonly ModernMTClient _client;
         public readonly MemoryServices Memories;
 
-        public ModernMTService(string apiKey, string platform = "modernmt-dotnet", string platformVersion = "1.0.0")
+        public ModernMTService(string apiKey, string platform = "modernmt-dotnet", string platformVersion = "1.0.1")
         {
             _client = new ModernMTClient(apiKey, platform, platformVersion);
             Memories = new MemoryServices(_client);
