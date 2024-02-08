b0c771f5f95be02670788cada040dc9d889a4f51
Davide Conti <davide.conti@modernmt.eu>
2024-02-08 16:09:38 +0100

    1.5.1

--- src/ModernMTService.cs
+++ src/ModernMTService.cs
@@ -16,7 +16,7 @@ namespace ModernMT
     public class ModernMTService
     {
         private const string Platform = "modernmt-dotnet";
-        private const string PlatformVersion = "1.5.0";
+        private const string PlatformVersion = "1.5.1";
 
         private readonly ModernMTClient _client;
         public readonly MemoryServices Memories;
